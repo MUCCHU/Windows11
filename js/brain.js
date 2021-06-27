99ef46502109cfce69ab66185702e3459eef43e5
unknown <harshwasnik160602@gmail.com>
2021-06-27 17:46:44 +0530

    start menu appears and dis-appears on clicking a button

--- js/brain.js
+++ js/brain.js
@@ -1,3 +1,17 @@
 function openStartMenu() {
-
+    // console.log("fxn called");
+    var div = document.getElementsByClassName("startMenu")[0];
+    var dispType = div.style.display;
+    if (dispType == "")
+        dispType = window.getComputedStyle(div).getPropertyValue("display");
+    if (dispType == "none") {
+        // console.log("The display type was ");
+        // console.log(div.style.display);
+        div.style.display = "flex";
+    }
+    else {
+        // console.log("The display type was ");
+        // console.log(div.style.display);
+        div.style.display = "none";
+    }
 }
