8c3ee3abb6f4a952fa8b3f89b5b7874cc8e4e386
unknown <harshwasnik160602@gmail.com>
2021-06-28 02:05:48 +0530

    Taskbar almost done

--- js/brain.js
+++ js/brain.js
@@ -15,3 +15,8 @@ function openStartMenu() {
         div.style.display = "none";
     }
 }
+async function addDate() {
+    var x = new Date();
+    document.getElementsByClassName("l2-time")[0].innerHTML = ((x.getHours() < 10) ? "0" + x.getHours() : x.getHours()) + ":" + ((x.getMinutes() < 10) ? "0" + x.getMinutes() : x.getMinutes());
+}
+setInterval(addDate, 1000);
