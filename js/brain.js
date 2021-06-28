c6cb08c2a029189dbb0d999ce48d19e3ef0d3876
unknown <harshwasnik160602@gmail.com>
2021-06-28 18:50:04 +0530

    transition added to start menu

--- js/brain.js
+++ js/brain.js
@@ -1,18 +1,10 @@
-function openStartMenu() {
-    // console.log("fxn called");
-    var div = document.getElementsByClassName("startMenu")[0];
-    var dispType = div.style.display;
-    if (dispType == "")
-        dispType = window.getComputedStyle(div).getPropertyValue("display");
-    if (dispType == "none") {
-        // console.log("The display type was ");
-        // console.log(div.style.display);
-        div.style.display = "flex";
+function transStartMenu() {
+    var startM = document.getElementsByClassName("startMenu")[0];
+    if (startM.style.bottom == "55px") {
+        startM.style.bottom = "-608px";
     }
     else {
-        // console.log("The display type was ");
-        // console.log(div.style.display);
-        div.style.display = "none";
+        startM.style.bottom = "55px";
     }
 }
 async function addDate() {
