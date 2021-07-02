98134438faaf6acace8d37b9380465109aafaf45
unknown <harshwasnik160602@gmail.com>
2021-07-02 20:21:36 +0530

    On clicking chrome window transitions but on finishing of start menu animation window gets launched

--- js/brain.js
+++ js/brain.js
@@ -7,6 +7,41 @@ function transStartMenu() {
         startM.style.bottom = "55px";
     }
 }
+function openWindow() {
+    var clickedIcon = true;
+    var div = document.getElementsByClassName("windowPopup")[0];
+    var computedStyles = window.getComputedStyle(div);
+    console.log(computedStyles.getPropertyValue("width"));
+    if (computedStyles.getPropertyValue("width") == "0px") {
+        transStartMenu();
+        $(".startMenu").on('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd',
+            function () {
+
+                // if (div.style.display == "block") {
+                //     div.style.display = "none";
+                // }
+                // else {
+                //     div.style.display = "block";
+                // }
+                if (computedStyles.getPropertyValue("width") == "0px")
+                    div.style.animation = "openUpWindow 0.3s forwards";
+                else {
+                    console.log("Want to close window");
+                    div.style.animation = "closeWindow 0.3s reverse forwards";
+                }
+            });
+    }
+    else {
+        if (computedStyles.getPropertyValue("width") == "0px")
+            div.style.animation = "openUpWindow 0.3s forwards";
+        else {
+            console.log("Want to close window");
+            div.style.animation = "closeWindow 0.3s forwards";
+        }
+
+    }
+    var clickedIcon = false;
+}
 async function addDate() {
     var x = new Date();
     document.getElementsByClassName("l2-time")[0].innerHTML = ((x.getHours() < 10) ? "0" + x.getHours() : x.getHours()) + ":" + ((x.getMinutes() < 10) ? "0" + x.getMinutes() : x.getMinutes());
